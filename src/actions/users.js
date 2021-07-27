6e4d011b4a5e2a0df8e068b52f73798d6c7c1277
mkaaslv <maria.kaasikaaslav@gmail.com>
2021-07-27 10:05:57 +0300

    main actions done

--- src/actions/users.js
+++ src/actions/users.js
@@ -15,7 +15,7 @@ export const createUser = (username, firstName, lastName, email) => async (dispa
     
         dispatch({
             type: CREATE_USER,
-            payload: res.data,
+            payload: res.data
           });
 
         return Promise.resolve(res.data);
@@ -23,3 +23,86 @@ export const createUser = (username, firstName, lastName, email) => async (dispa
         return Promise.reject(err);
     }
 };
+
+export const retrieveUsers = () => async (dispatch) => {
+    try {
+        const res = await UserDataService.getAll();
+
+        dispatch({
+            type: RETRIEVE_USERS,
+            payload: res.data
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const updateUser = (id, data) => async (dispatch) => {
+    try {
+        const res = await UserDataService.update(id, data);
+
+        dispatch({
+            type: UPDATE_USER,
+            payload: data
+        });
+
+        return Promise.resolve(res.data);
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
+
+export const deleteUser = (id) => async (dispatch) => {
+    try {
+        await UserDataService.remove(id);
+
+        dispatch({
+            type: DELETE_USER,
+            payload: { id }
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const deleteAllUsers = () => async (dispatch) => {
+    try {
+        const res = await UserDataService.removeAll();
+
+        dispatch({
+            type: DELETE_ALL_USERS,
+            payload: res.data
+        });
+
+        return Promise.resolve(res.data);
+    } catch (err) {
+        return Promise.reject(err);
+    }
+};
+
+export const findUserByEmail = (email) => async (dispatch) => {
+    try {
+        const res = await UserDataService.findByEmail(email);
+
+        dispatch({
+            type: RETRIEVE_USERS,
+            payload: res.data
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const findUserByUsername = (username) => async (dispatch) => {
+    try {
+        const res = await UserDataService.findByUsername(username);
+
+        dispatch({
+            type: RETRIEVE_USERS,
+            payload: res.data
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
